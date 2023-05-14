49eff2f1281aedb7a7ad89e938629ccb515f613b
LeRoyce Pearson <contact@leroycepearson.dev>
2023-05-14 14:20:45 -0600

    Create a dummy library to link against

--- build.zig
+++ build.zig
@@ -1,10 +1,19 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    const install_headers = b.addInstallDirectory(.{
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const opencl = b.addStaticLibrary(.{
+        .name = "OpenCL",
+        .root_source_file = .{ .path = "dummy.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    opencl.installHeadersDirectoryOptions(.{
         .source_dir = "CL",
         .install_dir = .header,
         .install_subdir = "CL",
     });
-    b.getInstallStep().dependOn(&install_headers.step);
+    b.installArtifact(opencl);
 }
